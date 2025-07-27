7f427de99cb076a4e255d150f630aa07f2cb2bf9
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-07-27 11:28:30 +0530

    Update indexing.py

--- Assignment-8/indexing.py
+++ Assignment-8/indexing.py
@@ -1,6 +1,9 @@
 from langchain_mistralai.chat_models import ChatMistralAI
+from dotenv import load_dotenv
+load_dotenv()
+
 def setup_llm():
     """Setup and return the LLM model"""
-    llm = ChatMistralAI(api_key="lHcwga2vJ6yyjV470WdMIFn5hRgtMbcc")
+    llm = ChatMistralAI()
     return llm
 
