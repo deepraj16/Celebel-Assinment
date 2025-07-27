ef6c0c1e0461be5383904348da537cafe29930c2
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-07-27 11:27:54 +0530

    Update vector_database.py

--- Assignment-8/vector_database.py
+++ Assignment-8/vector_database.py
@@ -1,10 +1,12 @@
 from langchain_community.vectorstores import FAISS
 from langchain_mistralai.embeddings import MistralAIEmbeddings
+from dotenv import load_dotenv
+load_dotenv()
 
 def setup_vectorstore(docs):
     """Setup and return the vector store"""
     embeddings = MistralAIEmbeddings(
-    api_key="lHcwga2vJ6yyjV470WdMIFn5hRgtMbcc",  
+   # api_key="",  // api key in evn  
     model="mistral-embed",                      
     )
     return FAISS.from_documents(docs, embeddings)
